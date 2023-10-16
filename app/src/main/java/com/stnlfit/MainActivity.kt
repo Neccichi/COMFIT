1e010fd7d3317795eea85d3cbbebc386f24d8924
Neccichi <timboniuk@gmail.com>
2023-10-16 17:13:58 +0300

    fix to navmenu

--- app/src/main/java/com/stnlfit/MainActivity.kt
+++ app/src/main/java/com/stnlfit/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.stnlfit
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomnavigation.LabelVisibilityMode
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Инициализируем BottomNavigationView
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
 
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -14,6 +24,26 @@ class MainActivity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
-        setContentView(R.layout.activity_main)
+
+        bottomNavigationView.labelVisibilityMode = NavigationBarView.LABEL_VISIBILITY_LABELED
+
+        // Настройка слушателя для обработки выбранных элементов меню
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.ic_home -> {
+                    // Обработка нажатия на "Головна"
+                }
+                R.id.ic_favorite -> {
+                    // Обработка нажатия на "Другорядна"
+                }
+                R.id.ic_search -> {
+                    // Обработка нажатия на "Поиск"
+                }
+                R.id.ic_profile -> {
+                    // Обработка нажатия на "Профіль"
+                }
+            }
+            true
+        }
     }
 }
